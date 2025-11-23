ef2e05ef8c89b8e33145a02d7bbb47d86eed2e58
nishantaMishra <nishantaMishra@users.noreply.github.com>
2025-11-22 20:33:33 -0500

    Exclude /admin routes from React SPA routing

--- client/App.tsx
+++ client/App.tsx
@@ -18,27 +18,34 @@ import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/resources" element={<Resources />} />
-          <Route path="/resources/pre-arrival" element={<PreArrival />} />
-          <Route path="/resources/post-arrival" element={<PostArrival />} />
-          <Route path="/resources/living-in-state-college" element={<LivingInStateCollege />} />
-          <Route path="/events" element={<Events />} />
-          <Route path="/forum" element={<Forum />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+const App = () => {
+  // Don't render React app for admin routes - let Netlify/static files handle it
+  if (window.location.pathname.startsWith('/admin/')) {
+    return null;
+  }
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/resources" element={<Resources />} />
+            <Route path="/resources/pre-arrival" element={<PreArrival />} />
+            <Route path="/resources/post-arrival" element={<PostArrival />} />
+            <Route path="/resources/living-in-state-college" element={<LivingInStateCollege />} />
+            <Route path="/events" element={<Events />} />
+            <Route path="/forum" element={<Forum />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+};
 
 createRoot(document.getElementById("root")!).render(<App />);
